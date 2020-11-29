d75e7db3db63f188deb109d86dfac8edc463a307
Lucas Rech <bymarreco@gmail.com>
2020-11-29 16:12:02 -0300

    Inserção DataContext

--- SmartSchool-WebAPI/Models/Professor.cs
+++ SmartSchool-WebAPI/Models/Professor.cs
@@ -1,19 +1,20 @@
+using System.Collections.Generic;
+
 namespace SmartSchool_WebAPI.Models
 {
     public class Professor
     {
         public Professor(){ }
 
-        public Professor(int id, string nome, string disciplina) 
+        public Professor(int id, string nome) 
         {
             this.id = id;
             this.nome = nome;
-            this.disciplina = disciplina;
                
         }
 
         public int id { get; set; }
         public string nome { get; set; }
-        public string disciplina { get; set; }
+        public IEnumerable<Disciplina> disciplinas { get; set; }
     }
 }
